0b966ef6e2744481b7e574a05625f53ffbc3f5c1
Jacob Trock <jtbosshku@gmail.com>
2021-01-27 16:05:46 -0500

    finish work on template formatter

--- inputprocessor/index.mjs
+++ inputprocessor/index.mjs
@@ -1,35 +1,29 @@
 import { v4 as uuidv4 } from 'uuid';
+import csv from 'csv-parser';
+import fs from 'fs';
+
 let ticker=1;
 let mlInput={};
 let serviceInput={};
-var standard_input = process.stdin;
 
-// Set input character encoding.
-standard_input.setEncoding('utf-8');
-
-// Prompt user to input data in console.
-console.log("enter query");
-
-// When user input data and click enter key.
-standard_input.on('data', function (data) {
-
-    // User input exit.
-    if(data === 'exit\n'){
-        // Program exit.
-        console.log(mlInput);
-        console.log(serviceInput);
-        process.exit();
-    }else
-    {
-        let id=uuidv4();
+fs.createReadStream('in.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    let id=uuidv4();
         mlInput[ticker.toString()]={
-            "invocation":data.split(",")[0],
+            "invocation":row.desc,
             "cmd":id
         };
         serviceInput[id]={
-            "clientScript":data.split(",")[1],
-            "backendCommand":data.split(",")[2]
+            "clientScript":row.clientComm,
+            "backendCommand":row.servComm
         };
         ticker++;
-    }
-});
+  })
+  .on('end', () => {
+    console.log("(MLI)===================================");
+    console.log(mlInput);
+    console.log("(ServInp)===================================");
+    console.log(serviceInput);
+    process.exit();
+  });
