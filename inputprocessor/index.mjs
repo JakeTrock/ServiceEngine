7f266817902e553652dbc6f3d6fcca4d531c1ef0
Jacob Trock <jtbosshku@gmail.com>
2021-01-27 23:12:52 -0500

    add fs save

--- inputprocessor/index.mjs
+++ inputprocessor/index.mjs
@@ -1,29 +1,39 @@
-import { v4 as uuidv4 } from 'uuid';
+import {
+  v4 as uuidv4
+} from 'uuid';
 import csv from 'csv-parser';
 import fs from 'fs';
 
-let ticker=1;
-let mlInput={};
-let serviceInput={};
+let ticker = 1;
+let mlInput = {};
+let serviceInput = {};
 
 fs.createReadStream('in.csv')
   .pipe(csv())
   .on('data', (row) => {
-    let id=uuidv4();
-        mlInput[ticker.toString()]={
-            "invocation":row.desc,
-            "cmd":id
-        };
-        serviceInput[id]={
-            "clientScript":row.clientComm,
-            "backendCommand":row.servComm
-        };
-        ticker++;
+    let id = uuidv4();
+    mlInput[ticker.toString()] = {
+      "invocation": row.desc,
+      "cmd": id
+    };
+    serviceInput[id] = {
+      "clientScript": row.clientComm,
+      "backendCommand": row.servComm
+    };
+    ticker++;
   })
   .on('end', () => {
-    console.log("(MLI)===================================");
-    console.log(mlInput);
-    console.log("(ServInp)===================================");
-    console.log(serviceInput);
-    process.exit();
+
+    fs.writeFile("nl2bash-data.json", mlInput, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("MLI was saved!");
+    });
+    fs.writeFile("svcInput.json", serviceInput, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("ServInp was saved!");
+    });
   });
