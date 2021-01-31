44f86afd7489405dc149a1a0bf91436f36dad90b
Jacob Trock <jtbosshku@gmail.com>
2021-01-31 09:32:51 -0500

    remove backend

--- uploadprocessor/dist/CommIndexes/assimp.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const assimpIndexes = ['', '', ''];
-exports.default = assimpIndexes;
-//# sourceMappingURL=assimp.js.map
--- uploadprocessor/dist/CommIndexes/uno.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const unoIndexes = ['', '', ''];
-exports.default = unoIndexes;
-//# sourceMappingURL=uno.js.map
