c1ca4121228d1a639ec7ca5fba2c5b8b7d64af1e
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2021-10-16 11:17:23 -0400

    Removing target blank

--- src/interface/allpage.tsx
+++ src/interface/allpage.tsx
@@ -6,7 +6,7 @@ const AllPage = () => {
   const gallery = [];
   for (let itm of allutils) {
     gallery.push(<div className="gallery">
-      <a target="_blank" href={`/runner/${itm.id}`} rel="noreferrer">
+      <a  href={`/runner/${itm.id}`} rel="noreferrer">
         {itm.name}
       </a>
       <div className="desc">{itm.description}</div>
